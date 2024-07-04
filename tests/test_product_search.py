48274216805d0ceb0a0706b25e7f2377928c9149
zhork9 <zhorazhuzha@gmail.com>
2024-07-04 21:27:12 +0200

    Implementation of "Product Navigation" test case with POM-model

--- tests/test_product_search.py
+++ tests/test_product_search.py
@@ -1,36 +1,25 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common import exceptions
 import pandas as pd
-from time import sleep
+
+from pages.home_page import HomePage
+
 
 def test_demon(_browser):
 
     wait = WebDriverWait(_browser, 10)
     url = "https://www.demoblaze.com"
-    data_csv = pd.read_table("data/data.csv", delimiter =",", float_precision='round_trip') 
+    data_csv = pd.read_table("data/data.csv", 
+                             delimiter =",", 
+                             float_precision='round_trip')
 
     _browser.get(url)
     _browser.maximize_window()
 
-    try:
-        open_cat = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='list-group'] a:nth-of-type(3)"))
-        )
-    except exceptions.ElementNotInteractableException as e:
-        print('{0} >> {1}'.format('open_cat', e))
-        
-    open_cat.click()
+    # initializers
+    home_page = HomePage(_browser)
     
-    _browser.implicitly_wait(1)
+    home_page.open_category(wait, 3)
 
-    try:
-        cat_laptop = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//a[contains(text(), '"+ data_csv.iloc[1, 1] +"')]"))
-        )
-    except exceptions.ElementNotVisibleException as e:
-        print('{0} >> {1}'.format('cat_laptop', e))
+    cat_prod = home_page.get_search_products(wait, data_csv.iloc[1, 1])
 
-    assert data_csv.iloc[1, 1] in cat_laptop.text
+    assert data_csv.iloc[1, 1] in cat_prod.text

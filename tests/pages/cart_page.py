e108bcdfbc2b9b9e5ee3beaf975b29728e4a9f9a
zhork9 <zhorazhuzha@gmail.com>
2024-07-04 21:26:24 +0200

    Page for POM-model

---
+++ tests/pages/cart_page.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common import exceptions
+
+class CartPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Returns table with 'price', 'name' and 'delete' button
+    def __get_table(self, wait):
+        try:
+            cart_table = wait.until(
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "tr.success td"))
+            )
+        except exceptions.ElementNotVisibleException as e:
+            print('{0} >> {1}'.format('cart_table', e))
+
+        return cart_table
+
+    # Returns product 'price' and 'name' in a list
+    def get_product_details(self, wait):
+        cart_table = self.__get_table(wait)
+
+        return [cart_table[1], cart_table[2]]
+
+    # Deletes product from cart
+    def delete_product(self, wait):
+        cart_table = self.__get_table(wait)
+        cart_table[3].find_element(By.CSS_SELECTOR, "a").click()
+
+    def verify_empty(self, wait):
+        try:
+            cart_empty = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "tr.success td"))
+            )
+            el_found = True
+        except exceptions.TimeoutException as e:
+            print('{0} >> {1}'.format('cart_empty', e))
+            el_found = False
+
+        return el_found
---
+++ tests/pages/home_page.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common import exceptions
+
+class HomePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Opens desired category
+    def open_category(self, wait, cat_num:int):
+        if cat_num > 1 and cat_num < 4:
+            try:
+                wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "div[class='list-group'] a:nth-of-type("
+                                            + str(cat_num) +")"))
+                                            ).click()
+            except exceptions.ElementNotInteractableException as e:
+                print('{0} >> {1}'.format('open_cat', e))
+        else:
+            return "No category with such index"
+
+    # Opens product page
+    def open_product(self, wait, name:str):
+        try:
+            wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), '"+ name +"')]"))
+            ).click()
+        except exceptions.ElementNotInteractableException as e:
+            print('{0} >> {1}'.format('product', e))
+
+    # Returns object with specified text
+    def get_search_products(self, wait, prod_name):
+        try:
+            cat_products = wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//a[contains(text(), '"+ prod_name +"')]")))
+        except exceptions.ElementNotVisibleException as e:
+            print('{0} >> {1}'.format('cat_products', e))
+        
+        return cat_products
